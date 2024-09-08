a58c369b852083a4be98adec79c873bfe70171ac
Eltfvzh <148296570+Eltfvzh@users.noreply.github.com>
2024-09-08 18:22:56 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,81 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="main-nav">
+        <div className="nav1">
+          <img src="Apple-logo-removebg-preview.png" alt="" />
+        </div>
+        <div className="nav2">
+          <p>main</p>
+          <p>content</p>
+          <p>footer</p>
+        </div>
+      </div>
+      <div className="main-content1">
+        <div className="container">
+          <div className="content">
+            <h1>Apple Club</h1>
+            <img src="Wallpaper-Laptop.webp" alt="" />
+          </div>
+          <div className="content1">
+            <h3>lorem</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus enim, at totam, ab dolores nihil sapiente non quas repellat, accusamus dolorum animi labore reprehenderit ex iste ipsum architecto molestias autem. Suscipit saepe tempora, necessitatibus rerum non at dignissimos soluta ut quaerat sed, cum odio. Enim nesciunt rerum blanditiis error?</p>
+          </div>
+        </div>
+      </div>
+      <div className="main-content2">
+        <div className="container1">
+          <div className="content2">
+            <h3>Lorem</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum voluptates accusamus, quasi sequi autem aperiam reprehenderit voluptatem in fugiat at enim cumque dolore quisquam quam blanditiis quia? Vitae aspernatur harum cupiditate ab. Vitae, eveniet voluptatibus! Id animi quod impedit modi.</p>
+          </div>
+          <div className="content3">
+            <h1>New Product</h1>
+            <img src="hp.jpg" alt="" />
+          </div>
+        </div>
+      </div>
+      <div className="more-content">
+        <div className="more1">
+          <img src="headset.jpg" alt="" />
+        </div>
+        <div className="more2">
+          <h1>more2</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam velit consectetur cumque quas, labore assumenda ipsa accusantium quo qui perspiciatis inventore iure veniam quibusdam praesentium delectus deserunt beatae nemo nostrum.</p>
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer1">
+          <h3>footer</h3>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+        </div>
+        <div className="footer2">
+          <h3>footer</h3>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+        </div>
+        <div className="footer3">
+          <h3>footer</h3>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+          <span>footer</span>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
